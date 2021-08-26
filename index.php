fe6079c54da646dffbeb505e3d781abea81bb3aa
Hassan K. Alkhalidi <gruceing@gmail.com>
2021-08-26 21:58:11 +0300

    Made some changes

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    // key => value
+    $human = [
+        'name' => "Mohammed",
+        'age' => 25,
+        'cars' => [
+            'Kia',
+            'BMW'
+        ],
+    ];
+    
+    foreach ($human as $element){
+        if (is_array($element))
+            foreach ($element as $car)
+                echo $car . '<br/>';
+        else
+            echo $element . "<br />";
+        
+    }
+
+?>
